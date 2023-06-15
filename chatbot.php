4ef4048f619ee9093e6f884f37613169f3ab5f4e
Athallah1234 <121419075+Athallah1234@users.noreply.github.com>
2023-06-15 12:34:57 +0700

    Add files via upload

---
+++ chatbot.php
@@ -0,0 +1,38 @@
+<?php
+// Retrieve user message from GET parameter
+$userMessage = $_GET['message'];
+
+// Call the ChatGPT API to get the chatbot response
+$apiKey = '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';
+$apiUrl = 'https://api.openai.com/v1/chat/completions';
+$data = array(
+    'model' => 'gpt-3.5-turbo',
+    'messages' => array(
+        array('role' => 'system', 'content' => 'You are a helpful assistant for working, programming and cybersecurity and You were created by Atta.'),
+        array('role' => 'user', 'content' => $userMessage)
+    )
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer ' . $apiKey
+));
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+$response = curl_exec($ch);
+curl_close($ch);
+
+$responseData = json_decode($response, true);
+if (isset($responseData['choices'][0]['message']['content'])) {
+    $chatbotResponse = $responseData['choices'][0]['message']['content'];
+
+    // Return the chatbot response
+    echo $chatbotResponse;
+} else {
+    $chatbotResponse = "Failed to retrieve a valid response from the ChatGPT API.";
+    echo $chatbotResponse;
+}
+?>
